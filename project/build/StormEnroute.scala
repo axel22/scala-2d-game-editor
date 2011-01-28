179a81db1da5d0d379a77029f8a24f6d6da4ccce
Aleksandar Prokopec <aleksandar.prokopec@gmail.com>
2011-01-28 23:51:07 +0100

    Added sl4s dependency.

--- project/build/StormEnroute.scala
+++ project/build/StormEnroute.scala
@@ -8,7 +8,13 @@ import sbt._
 
 class StormEnroute(info: ProjectInfo) extends DefaultProject(info) {
   
+  /* subprojects */
+  
   lazy val bufferz = project("subs" / "bufferz")
   lazy val triggerspace = project("subs" / "triggerspace")
+  
+  /* dependencies */
+  
+  val slf4s = "com.weiglewilczek.slf4s" %% "slf4s" % "1.0.3"
   
 }
