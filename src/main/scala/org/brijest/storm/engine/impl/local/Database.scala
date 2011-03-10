cb597efa863c32c96e847cd1414fa1066a5b6850
Aleksandar Prokopec <aleksandar.prokopec@gmail.com>
2011-03-10 21:08:03 +0100

    Database fixes.

--- src/main/scala/org/brijest/storm/engine/impl/local/Database.scala
+++ src/main/scala/org/brijest/storm/engine/impl/local/Database.scala
@@ -11,10 +11,11 @@ import model.{Area, AreaId, PlayerId}
 import Simulators.State
 
 
-class Database(config: Config) {
+class Database(config: Config) extends Logging {
+  import logger._
   
   val filename = "%s/%s".format(config.basedir, config.savename)
-  val existing = (new File(filename)).exists
+  val existing = (new File(filename + ".h2.db")).exists
   val url = "jdbc:h2:%s;FILE_LOCK=FS;PAGE_SIZE=1024;CACHE_SIZE=8192".format(filename)
   Class.forName("org.h2.Driver")
   val conn = java.sql.DriverManager.getConnection(url)
@@ -32,7 +33,7 @@ class Database(config: Config) {
   }
   
   if (!existing) {
-    update("create database %s".format(Database.name))
+    debug("creating new database")
     update("create table %s (%s bigint not null, %s blob, %s blob, primary key (%s))".format(tablename, areaid, data, statedata, areaid))
     update("create table %s (%s bigint not null, %s bigint not null, primary key (%s))".format(playerpos, pidcol, aidcol, pidcol))
   }
