c9fd5cf3b42693f38a62b51ce67a55accf01603c
Aleksandar Prokopec <axel22@gmail.com>
2012-07-10 19:59:45 +0200

    Argument parser fixes.

--- src/main/scala/org/brijest/storm/DefaultParser.scala
+++ src/main/scala/org/brijest/storm/DefaultParser.scala
@@ -15,8 +15,8 @@ import org.github.scopt._
 
 
 abstract class DefaultParser(appname: String) extends OptionParser(appname) {
-  opt("Srender.outline", "A boolean indicating whether to draw an outline only.", { v: String => app.render.outline(v.toBoolean) })
-  opt("Srender.shadows", "A boolean indicating whether to draw an outline only.", { v: String => app.render.shadows(v.toBoolean) })
+  opt("Srender.outline", "A boolean indicating whether to draw an outline only", { v: String => app.render.outline(v.toBoolean) })
+  opt("Srender.shadows", "A boolean indicating whether to draw shadows", { v: String => app.render.shadows(v.toBoolean) })
 }
 
 
